fe1fa982dbe466a33d6530ede2dc9b8401078d9d
Ray Reside <scrambled.humor@gmail.com>
2020-02-20 23:59:22 -0500

    Updates

---
+++ precache-manifest.a665c0e5c09160c1fe9275e844c375fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b412ef30f1871bdf23f47deca2cd4a7c",
+    "url": "/facerec/index.html"
+  },
+  {
+    "revision": "70a69e208ad9a33db04b",
+    "url": "/facerec/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0dbc92a9d23b93a42482",
+    "url": "/facerec/static/css/main.494ff37e.chunk.css"
+  },
+  {
+    "revision": "70a69e208ad9a33db04b",
+    "url": "/facerec/static/js/2.4749a937.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/facerec/static/js/2.4749a937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbc92a9d23b93a42482",
+    "url": "/facerec/static/js/main.7ec52e44.chunk.js"
+  },
+  {
+    "revision": "3ae9483801256733ca39",
+    "url": "/facerec/static/js/runtime-main.181b30ef.js"
+  }
+]);
